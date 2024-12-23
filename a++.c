6acb74e602870f053258bc604e0e6a8765b87db5
chetan <chetan.sq05@gmail.com>
2024-12-23 09:46:27 +0530

    23-12-2024

---
+++ a++.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int a = 15;
+printf("a:%d,a:%d,a:%d,a:%d,a:%d,a:%d,a:%d",a++,++a,a,++a,a++,a,++a);
+// printf("a: %d, a: %d, a: %d, a: %d", a++, ++a, a++, ++a);
+// printf("a: %d, a: %d, a: %d, a: %d", a, a++, a++, ++a);
+// printf("a: %d, a: %d, a: %d, a: %d", a, a++, a++, a++);
+// printf("a: %d, a: %d, a: %d", ++a, ++a, ++a);
+// printf("a: %d, a: %d, a: %d", a++, ++a, a++);
+// printf("a: %d, a: %d, a: %d", a++, a++, a++);
+// printf("a: %d, a: %d, a: %d", a, a++, a++);
+// printf("a: %d, a: %d", ++a, ++a);
+// printf("a: %d, a: %d",a++, ++a);
+// printf("a: %d, a: %d",a++, a++);
+// printf("a: %d, a: %d",a, a++);
+// printf("a: %d", ++a);
+}
---
+++ and_or.c
@@ -0,0 +1,41 @@
+//AND => when first digit 0 than no check condition
+//OR => when first digit 1 than no check condition
+#include <stdio.h>
+int main()
+{
+int a = 0, b = 1,c = 1, d;
+//d = ++a && ++b || c;
+//printf("a: %d, b: %d, c: %d, d: %d", a, b, c, d);
+
+//d = a++ && ++b && c;
+//printf("a: %d, b: %d, c: %d, d: %d", a, b, c, d);
+
+//int a = 1, b = 2,c;
+// c = ++a || ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = ++a && ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = a || ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = --a || ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = a || b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = a && ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c=a && b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = a && ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+// c = ++a && ++b;
+// printf("a: %d, b: %d, c: %d", a,b,c);
+
+}
