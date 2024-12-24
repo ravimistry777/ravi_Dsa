15650e0e646d04d0e449d5572901760345dfea09
chetan <chetan.sq05@gmail.com>
2024-12-24 08:10:32 +0530

    24-12-2024

--- a++.c
+++ a++.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(){
 int a = 15;
-printf("a:%d,a:%d,a:%d,a:%d,a:%d,a:%d,a:%d",a++,++a,a,++a,a++,a,++a);
-// printf("a: %d, a: %d, a: %d, a: %d", a++, ++a, a++, ++a);
-// printf("a: %d, a: %d, a: %d, a: %d", a, a++, a++, ++a);
-// printf("a: %d, a: %d, a: %d, a: %d", a, a++, a++, a++);
-// printf("a: %d, a: %d, a: %d", ++a, ++a, ++a);
-// printf("a: %d, a: %d, a: %d", a++, ++a, a++);
-// printf("a: %d, a: %d, a: %d", a++, a++, a++);
-// printf("a: %d, a: %d, a: %d", a, a++, a++);
-// printf("a: %d, a: %d", ++a, ++a);
-// printf("a: %d, a: %d",a++, ++a);
-// printf("a: %d, a: %d",a++, a++);
-// printf("a: %d, a: %d",a, a++);
-// printf("a: %d", ++a);
+printf("a:- %d, a:- %d, a:- %d, a:- %d, a:- %d, a:- %d, a:- %d\n",a++,++a,a,++a,a++,a,++a);
+printf("a:- %d, a:- %d, a:- %d, a:- %d\n", a++, ++a, a++, ++a);
+printf("a:- %d, a:- %d, a:- %d, a:- %d\n", a, a++, a++, ++a);
+printf("a:- %d, a:- %d, a:- %d, a:- %d\n", a, a++, a++, a++);
+printf("a:- %d, a:- %d, a:- %d\n", ++a, ++a, ++a);
+printf("a:- %d, a:- %d, a:- %d\n", a++, ++a, a++);
+printf("a:- %d, a:- %d, a:- %d\n", a++, a++, a++);
+printf("a:- %d, a:- %d, a:- %d\n", a, a++, a++);
+printf("a:- %d, a:- %d\n", ++a, ++a);
+printf("a:- %d, a:- %d\n",a++, ++a);
+printf("a:- %d, a:- %d\n",a++, a++);
+printf("a:- %d, a:- %d\n",a, a++);
+printf("a:- %d\n", ++a);
 }
